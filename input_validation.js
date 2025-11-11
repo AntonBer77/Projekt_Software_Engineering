bb5be8fbda95f010c56004d9092b100a6509e170
Anton Bersch <anton-bersch@gmx.de>
2025-11-11 17:40:10 +0100

    login validation implemented

--- input_validation.js
+++ input_validation.js
@@ -1,10 +1,53 @@
-const loginForm = document.getElementById("loginForm");
+const users = [
+  {username: "tester", email: "tester@iu.org", password: "1234"},
+  {username: "tutor", email: "tutor@iu.org", password: "tutor1234567"}
+];
 
-console.log(loginForm);
 
+const loginForm = document.querySelector("#login-form");
+const loginEmail = document.querySelector("#login-e-mail");
+const loginPassword = document.querySelector("#login-password");
+
+let errorMessageActive = false;
+let inputFieldErrorMessage;
 
 loginForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log('form submitted');
+  const formData = new FormData(loginForm);
+  const entries = Object.fromEntries(formData.entries());
+  const userEmail = entries.userEmail;
+  const userPassword = entries.userPassword;
+
+  let validEmail = false;
+
+  users.forEach((u) => {
+    if(u.email === userEmail) {
+      validEmail = true;
+    }
+  });
+
+  const validUser = users.find((user) => {    
+    return user.email === userEmail
+  });
+
+
+  if(errorMessageActive) {
+    inputFieldErrorMessage.classList.toggle('hidden');
+    errorMessageActive = false;
+  }
+  
+  if(!validEmail) {
+    errorMessageActive = true;
+    inputFieldErrorMessage = loginEmail.nextElementSibling;
+    loginEmail.nextElementSibling.classList.toggle('hidden');
+  } 
+  else if(validUser.password !== userPassword) {
+    errorMessageActive = true;
+    inputFieldErrorMessage = loginPassword.nextElementSibling; 
+    loginPassword.nextElementSibling.classList.toggle('hidden');
+  } else {
+    location.assign('home.html');
+  }
+
   
 })
